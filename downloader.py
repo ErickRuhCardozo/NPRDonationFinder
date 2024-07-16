f56714e8de5382c5359b364a2511c528a440b2c4
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2024-07-16 14:28:25 -0300

    Modified module behaviour
    Now the download only occurs when there is more than 0 donations for a specified month.
    The new update also makes use of a temporary file instead of downloading the donation report in the app directory.

--- downloader.py
+++ downloader.py
@@ -5,11 +5,30 @@
 from requests import Session
 from datetime import date
 from tempfile import NamedTemporaryFile
+import json
+
+
+def get_donations_count(session: Session, date: date) -> int:
+    params = {'mes': date.month, 'ano': date.year, 'draw': 1, 'start': 0, 'length': 1}
+    response = session.get('https://notaparana.pr.gov.br/nfprweb/app/v1/datatable/documentoFiscalDoado/', params=params)
+
+    if not response.ok:
+        return 0
+
+    response = json.loads(response.text)
+    return response['recordsTotal']
 
 
 def download_donations(session: Session, date: date) -> bool | str:
+    """
+    Attempts to download a donation report. On success, returns the
+    path to the downloaded report. If the request status code is not
+    200 or the total records for the queried date are 0, returns False.
+    """
+    if get_donations_count(session, date) == 0:
+        return False
+
     period = f'{date.month:02}/{date.year}'
-    filename = f'{period.replace('/', '-')}.csv'
     url = f'https://notaparana.pr.gov.br/nfprweb/RelatorioDocFiscalDoado?periodo={period}'
     print(f'Attempting download donation resume for period: {period}')
     with session.get(url, stream=True) as response:
@@ -17,10 +36,8 @@ def download_donations(session: Session, date: date) -> bool | str:
             print(f'Could not download resource: Server returned {response.status_code}')
             return False
         
-        with open(filename, 'wb') as f:
-            print(f'Writing to file: {filename}')
+        with NamedTemporaryFile('wb', delete=False) as f:
+            print(f'Writing to file: {f.name}')
             for chunk in response.iter_content(10  * 1024):
                 f.write(chunk)
-    
-    print('Donation report successfully downloaded!')
-    return filename
+            return f.name
