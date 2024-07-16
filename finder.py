130a9a65f34ffe4841334c1d1b6699c4dc9f041b
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2024-07-16 14:30:51 -0300

    Added Pandas library
    This new version uses Pandas to process the donation data.
    The module was also structured into functions, instead of the scripted approach before.

--- finder.py
+++ finder.py
@@ -2,51 +2,70 @@
 # Date: Jul 15, 2024 - 13:55 PM
 # Description: This module attempts to find the last time an EIN appears in a donation.
 
-from csv import reader as csv_reader
-from auth import User, UserDecoder, login
-from downloader import download_donations
+import json
+import auth
+import pandas as pd
+import downloader as dl
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from os import rename
-from json import load, JSONDecoder
+from dateutil.relativedelta import relativedelta as datedelta
+from pprint import PrettyPrinter
 
 
 EIN_SOUGHT = ''
-
-with open('users.json') as f:
-    users = load(f, cls=UserDecoder)
+MAX_DOWNLOAD_ATTEMPTS = 3 # How many download attempts in a row before skipping to next user
 
 
-for user in users:
-    if (session := login(user)) == False:
-        print(f'Could not login user: {user.name}')
-        continue
+def load_users() -> list[auth.User]:
+    with open('./users.json') as f:
+        return json.load(f, cls=auth.UserDecoder)
 
-    date = datetime.today()
 
-    while date.month != 1:
-        if (filename := download_donations(session, date)) == False:
-            print(f'Could not download donations of user "{user.name}" for the period {date}')
+def wrangle_donations(df: pd.DataFrame):
+    df.drop(columns=['CNPJ', 'ENTIDADE', 'SITUAÇÃO'], inplace=True)
+    df['NOTA'] = df['NOTA'].apply(lambda x: x[6:20])
+    df['NOTA'] = df['NOTA'].apply(lambda x: f'{x[0:2]}.{x[2:5]}.{x[5:8]}/{x[8:12]}-{x[12:]}')
+    df.rename(columns={'NOTA': 'CNPJ', 'DATA DA DOAÇÃO': 'DATA'}, inplace=True)
+    df.drop_duplicates(keep='first', subset=['CNPJ'], inplace=True)
+    #df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y')
+
+
+def main():
+    users = load_users()
+    donations = [] # Date of donations were EIN_SOUGHT was found
+
+    for user in users:
+        if (session := auth.login(user)) == False:
+            print(f'Could not log-in user: {user.name}. Skipping...')
             continue
 
-        new_filename = f'{user.name} - {filename}'
-        rename(filename, new_filename)
+        donations.append({user.name: []})
+        date = datetime.today()
+        failed_download_attempts = 0
 
-        with open(new_filename) as f:
-            found_dates = set()
-            reader = csv_reader(f, delimiter=';')
-            for row in reader:
-                if reader.line_num < 3: # Skip headers
-                    continue
+        while failed_download_attempts < MAX_DOWNLOAD_ATTEMPTS:
+            if (filename := dl.download_donations(session, date)) == False:
+                print(f'Could not download donations of user: {user.name}, for the period: {date}. Skipping...')
+                date = date - datedelta(months=1)
+                failed_download_attempts += 1
+                continue
 
-                access_key = row[2]
-                ein = access_key[6:20]
-                formated_ein = f'{ein[:2]}.{ein[2:5]}.{ein[5:8]}/{ein[8:12]}-{ein[12:]}'
+            df = pd.read_csv(filename, encoding='iso8859-1', skiprows=1, sep=';')
+            wrangle_donations(df)
 
-                if EIN_SOUGHT == formated_ein and row[3] not in found_dates:
-                    found_dates.add(row[3])
-                    print(f'FOUND DONATION FROM USER: {user.name}, IN: {row[3]}')
+            if EIN_SOUGHT in df['CNPJ'].values:
+                row = df.query(f'CNPJ == "{EIN_SOUGHT}"').index[0]
+                donations[-1][user.name].append(df.loc[row, 'DATA'])
 
-        date = date - relativedelta(months=1)
+            date = date - datedelta(months=1)
+    
+    for d in donations:
+        for k in d:
+            d[k].reverse()
 
-    print(f'Finished cycle for user: {user.name}')
+    print('Finished Look Up.')
+    print('Donation Dates:')
+    PrettyPrinter().pprint(donations)
+
+
+if __name__ == '__main__':
+    main()
